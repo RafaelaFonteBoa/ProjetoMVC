8816b5e4fbdc3e786a50749b0a088e8b12873ae6
Thiago Suchorski <suchorski@ymail.com>
2023-03-02 09:07:52 -0300

    Identação ajustada

--- ProjetoMVC/app/controller/SobreController.php
+++ ProjetoMVC/app/controller/SobreController.php
@@ -1,22 +1,19 @@
-<?php 
-	/**
-	 * 
-	 */
-	class SobreController
-	{
-		
-		public function index(){
+<?php
+/**
+ *
+ */
+class SobreController
+{
+    public function index()
+    {
+        $loader = new \Twig\Loader\FilesystemLoader('app/view'); //caminho do nosso template
+        $twig = new \Twig\Environment($loader); //objeto ENVIRONMENT do twig vai carregar nosso template
+        $template = $twig->load('sobre.html'); //e vai rodar nosso template
 
+        $parametros = array();
 
-				$loader = new \Twig\Loader\FilesystemLoader('app/view'); //caminho do nosso template
-				$twig = new \Twig\Environment($loader); //objeto ENVIRONMENT do twig vai carregar nosso template
-				$template = $twig->load('sobre.html'); //e vai rodar nosso template
+        $conteudo = $template->render($parametros); //renderização do template com os parametros
 
-				$parametros = array();
-
-				$conteudo = $template->render($parametros); //renderização do template com os parametros
-
-				echo $conteudo; 
-		}
-	}
-?>
+        echo $conteudo;
+    }
+}
--- ProjetoMVC/app/lib/Database/conecta.php
+++ ProjetoMVC/app/lib/Database/conecta.php
@@ -1,15 +1,14 @@
-<?php 
-	abstract class Connection
-	{
-		
-		private static $conn;
+<?php
+abstract class Connection
+{
+    private static $conn;
 
-		public static function getConn(){
-			if (self::$conn == NULL) {
-				self::$conn = new PDO('mysql: host=127.0.0.1; dbname=bd_site_teste;','rafaela','oie1232');
-			}
-			
-			return self::$conn;
-		}
-	}
-?>
+    public static function getConn()
+    {
+        if (self::$conn == null) {
+            self::$conn = new PDO('mysql: host=127.0.0.1; dbname=bd_site_teste;', 'rafaela', 'oie1232');
+        }
+
+        return self::$conn;
+    }
+}
